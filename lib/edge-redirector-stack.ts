407ccc32d165d39676935ce559397a4d0942df38
James Mead <james@floehopper.org>
2020-12-30 20:13:20 +0000

    Add domain aliases for floehopper.org & blog.floehopper.org
    This in turn required me to specify an SSL certificate for each domain
    in order to avoid errors like this one:
    
    > To add an alternate domain name (CNAME) to a CloudFront distribution, you must attach a trusted certificate that validates your authorization to use the domain name. For more details, see:
    https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/CNAMEs.html#alternate-domain-names-requirements (Service: AmazonCloudFront; Status Code: 400; Error Code: InvalidViewerCertificate; Request ID: fe64a158-8fab-4f26-adb3-b55f8dc61c09; Proxy: null)
    
    I created the certificates using the recently added
    create-ssl-certificate.sh script. It seemed simpler to create them in a
    script rather than using a separate CDK stack.

--- lib/edge-redirector-stack.ts
+++ lib/edge-redirector-stack.ts
@@ -2,17 +2,19 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as acm from '@aws-cdk/aws-certificatemanager';
 
 export class EdgeRedirectorStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // this.createDistribution('hannahsmithson.org', 'hannahsmithsonOrg.handler');
-    this.createDistribution('floehopper.org', 'floehopperOrg');
-    this.createDistribution('blog.floehopper.org', 'blogFloehopperOrg');
+    this.createDistribution('floehopper.org', 'floehopperOrg', 'arn:aws:acm:us-east-1:687105911108:certificate/9f1d191d-aa36-4dac-85f2-f60684a62b65');
+    this.createDistribution('blog.floehopper.org', 'blogFloehopperOrg', 'arn:aws:acm:us-east-1:687105911108:certificate/aa11ee5a-54db-4a04-8307-f77330f86cb5');
   }
 
-  createDistribution(domain: string, handler: string) {
+  createDistribution(domain: string, handler: string, certificateArn: string) {
+    const certificate = acm.Certificate.fromCertificateArn(this, `${handler}Certificate`, certificateArn);
     new cloudfront.Distribution(this, `${handler}Distribution`, {
       defaultBehavior: {
         origin: new origins.HttpOrigin('example.com'),
@@ -23,6 +25,8 @@ export class EdgeRedirectorStack extends cdk.Stack {
           }
         ]
       },
+      domainNames: [domain],
+      certificate: certificate,
       enableLogging: true
     });
   }
