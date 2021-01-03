18f7c4160db688900f0e024c5ca35366a82adea7
James Mead <james@floehopper.org>
2021-01-03 12:31:42 +0000

    Add CloudFront distribution for feeds.floehopper.org
    This by-passes FeedBurner and redirects directly to the raw feeds.

--- lib/edge-redirector-stack.ts
+++ lib/edge-redirector-stack.ts
@@ -12,6 +12,7 @@ export class EdgeRedirectorStack extends cdk.Stack {
     this.createDistribution('floehopper.org', 'floehopperOrg', 'arn:aws:acm:us-east-1:687105911108:certificate/9f1d191d-aa36-4dac-85f2-f60684a62b65');
     this.createDistribution('www.floehopper.org', 'wwwFloehopperOrg', 'arn:aws:acm:us-east-1:687105911108:certificate/64224aef-5ab9-47f4-8828-c87ed3fa9187');
     this.createDistribution('blog.floehopper.org', 'blogFloehopperOrg', 'arn:aws:acm:us-east-1:687105911108:certificate/aa11ee5a-54db-4a04-8307-f77330f86cb5');
+    this.createDistribution('feeds.floehopper.org', 'feedsFloehopperOrg', 'arn:aws:acm:us-east-1:687105911108:certificate/abb2fd4c-b68f-4563-831d-af74ae403e37');
   }
 
   createDistribution(domain: string, handler: string, certificateArn: string) {
